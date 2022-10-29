d5b289a0d393d175e3cd4649b6473b2525028f55
Andy Couto <andrea.couto@panerabread.com>
2022-10-29 13:48:00 -0400

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Intro from "./components/Intro/Intro";
 import NamePairEULA from "./components/NamePair/NamePairEULA";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
@@ -10,8 +10,8 @@ const App = () => {
   return <Router>
   <div>
     <Routes>
-      <Route exact path='/' element={<Intro/>} />
-      <Route exact path='/NamePairEULA' element={<NamePairEULA/>} />
+      <Route path='/' element={<Intro/>} />
+      <Route path='/NamePairEULA' element={<NamePairEULA/>} />
     </Routes>
   </div>
 </Router>;
